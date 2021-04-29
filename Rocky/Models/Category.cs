9295dd50d8e09e5d4c4ff09d1d7aaf05e1941a41
Siraj <sgsdbs@gmail.com>
2021-04-29 23:30:55 +0300

    Добавил класс Category

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+	public class Category
+	{
+		public int CategoryId { get; set; }
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+
+
+	}
+}
