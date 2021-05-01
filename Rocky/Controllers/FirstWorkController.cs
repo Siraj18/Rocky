e7774ffcbe9b12c22329c6c68243bc24e915bbb4
Siraj <sgsdbs@gmail.com>
2021-05-02 01:18:17 +0300

    Реализовал CRUD для FirstWork

--- Rocky/Controllers/FirstWorkController.cs
+++ Rocky/Controllers/FirstWorkController.cs
@@ -39,5 +39,50 @@ namespace Rocky.Controllers
 
             return View();
         }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.FirtsWorks.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(FirtsWork obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.FirtsWorks.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            
+            var obj = _db.FirtsWorks.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.FirtsWorks.Remove(obj);
+            _db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
     }
 }
