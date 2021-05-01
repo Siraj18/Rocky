5741cfca7fbd8d7895f30abd8c489fb553b2d09b
Siraj <sgsdbs@gmail.com>
2021-05-01 19:23:52 +0300

    Реализация добавления новых категорий в базу данных

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -20,5 +20,20 @@ namespace Rocky.Controllers
             IEnumerable<Category> objList = _db.Categories;
             return View(objList);
         }
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
