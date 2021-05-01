ad9e7ae73d20d51be1b34c58f6f059fc1b3ff356
Siraj <sgsdbs@gmail.com>
2021-05-02 01:04:08 +0300

    Добавил валид

--- Rocky/Models/FirtsWork.cs
+++ Rocky/Models/FirtsWork.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Rocky.Models
 {
 	public class FirtsWork
 	{
 		public int Id { get; set; }
-		
+		[Required]
 		public String Name { get; set; }
 	}
 }
