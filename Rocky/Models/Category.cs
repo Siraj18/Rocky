deeeebac8772d8317aceb7631e3ebd691410945f
Siraj <sgsdbs@gmail.com>
2021-05-02 00:15:01 +0300

    Сделал валидацию для добавления категорий

--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Rocky.Models
 {
 	public class Category
 	{
 		public int CategoryId { get; set; }
+		[Required]
 		public string Name { get; set; }
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "Display Order must be grater than 0")]
 		public int DisplayOrder { get; set; }
 
 
