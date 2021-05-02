bf96b9131fb40e73a9337e93f05951b7f343d8c8
Siraj <sgsdbs@gmail.com>
2021-05-03 01:41:03 +0300

    Начало работы над выводом продуктов. Создана таблица, миграции, а также представление Index.

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Products;
+
+            foreach(var obj in objList)
+            {
+                obj.Category = _db.Categories.FirstOrDefault(u => u.CategoryId == obj.CategoryId);
+            }
+
+            return View(objList);
+        }
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace Rocky.Data
 
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<FirtsWork> FirtsWorks { get; set; }
+		public DbSet<Product> Products { get; set; }
 	}
 }
