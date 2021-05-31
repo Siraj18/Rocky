ac8dd3d795cae865c32628e6dd6550551bbc079d
Siraj <sgsdbs@gmail.com>
2021-06-01 00:23:50 +0300

    Добавил mailjet к проекту

--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Rocky.Data;
+using Rocky.Utility;
 
 namespace Rocky
 {
@@ -31,6 +33,7 @@ namespace Rocky
 			options.UseSqlServer(
 				Configuration.GetConnectionString("DefaultConnection")
 				));
+			services.AddTransient<IEmailSender, EmailSender>();
 			services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI().AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
 			services.AddHttpContextAccessor();
---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,59 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+	public class EmailSender : IEmailSender
+	{
+		public Task SendEmailAsync(string email, string subject, string htmlMessage)
+		{
+			return Execute(email, subject, htmlMessage);
+		}
+
+		public async Task Execute(string email, string subject, string body)
+		{
+            MailjetClient client = new MailjetClient("fb24fc8e4e7e8893165beed59708641a", "6fdd9e28a6ab9b783d86c4710b05a973");
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "sgsdbs@gmail.com"},
+        {"Name", "Siraj"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "DotNetMastery"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      }, {
+       "HTMLPart",
+       body
+      },
+     }
+             });
+            await client.PostAsync(request);
+        }
+	}
+}
